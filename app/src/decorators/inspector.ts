4c086be9ede0914336c66fbf6164e63e2bc96057
Henrique Melo <hmelo2509@gmail.com>
2022-03-23 16:09:46 +0000

    refactor: removido wrapper do decorator inspect

--- app/src/decorators/inspector.ts
+++ app/src/decorators/inspector.ts
@@ -1,15 +1,14 @@
-export function inspect() {
-    return function(
-        target: any,
-        propertyKey: string,
-        descriptor: PropertyDescriptor
-    ) {
-        const func = descriptor.value
-        descriptor.value = function(...args: Array<any>) {
-            console.log(`-- Método ${propertyKey}(${JSON.stringify(args)}) --`)
-            const retorno = func.apply(this, args)
-            console.log(`Retorna: ${retorno}`)
-            retorno
-        }
+export function inspect(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+) 
+{
+    const func = descriptor.value
+    descriptor.value = function(...args: Array<any>) {
+        console.log(`-- Método ${propertyKey}(${JSON.stringify(args)}) --`)
+        const retorno = func.apply(this, args)
+        console.log(`Retorna: ${retorno}`)
+        retorno
     }
 }
