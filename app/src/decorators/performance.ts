82dd074613f77d9ac56878cd95e71534abc49a7e
Henrique Melo <hmelo2509@gmail.com>
2022-03-23 15:29:19 +0000

    feature: performance decorator para testar performance de métodos

---
+++ app/src/decorators/performance.ts
@@ -0,0 +1,16 @@
+export function checkPerformance() {
+    return function(
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        const func = descriptor.value
+        descriptor.value = function(...args: Array<any>) {
+            const t1 = performance.now()
+            const retorno = func.apply(this, args)
+            const t2 = performance.now()
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`)
+            retorno
+        }
+    }
+}
